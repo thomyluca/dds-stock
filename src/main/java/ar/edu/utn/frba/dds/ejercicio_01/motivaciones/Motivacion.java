28ea42da28e9b511e1cadf972654fd9f039d669e
Thomas Luca <tluca@frba.utn.edu.ar>
2024-09-17 20:44:20 -0300

    Limpieza

--- src/main/java/ar/edu/utn/frba/dds/ejercicio_01/motivaciones/Motivacion.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frba.dds.ejercicio_01.motivaciones;
-
-import ar.edu.utn.frba.dds.ejercicio_01.Deportista;
-import ar.edu.utn.frba.dds.ejercicio_01.Rutina;
-
-public interface Motivacion {
-    public Rutina generarRutinaPara(Deportista deportista);
-}
